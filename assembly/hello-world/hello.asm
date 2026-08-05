5a6d4dccaa1e49f7d7a69ad3f7527ac41cea8fe4
Erik Borela <erikborella@gmail.com>
2026-08-04 21:04:39 -0300

    Adicionado asm de Hello World

---
+++ assembly/hello-world/hello.asm
@@ -0,0 +1,23 @@
+bits 64
+
+%define WRITE_SYSCALL 1
+%define EXIT_SYSCALL 60
+
+%define STDOUT 1
+
+global _start
+
+section .data
+message: db 'Hello, world!', 10
+
+section .text
+_start:
+  mov rax, WRITE_SYSCALL
+  mov rdi, STDOUT
+  mov rsi, message
+  mov rdx, 14
+  syscall
+
+  mov rax, EXIT_SYSCALL
+  mov rdi, 0
+  syscall
