d0ebf47584fab7a23d09538db0be044e3286406b
Erik Borela <erikborella@gmail.com>
2026-08-11 01:27:24 -0300

    Adicionado print_uint na lib IO

--- assembly/io-lib/io-lib.asm
+++ assembly/io-lib/io-lib.asm
@@ -88,9 +88,56 @@ print_newline:
 
 ; ---
 
+; rdi -> uint
+print_uint:
+  ; rcx = indice da string que vamos construir
+  mov rcx, rsp
+  ; Alocalmos 21 bytes na stack
+  ; O máximo de digitos que um u64 tem é 20
+  ; + 1 para o caractere de fim de string (0)
+  sub rsp, 21
+
+  ; Construimos a string começando com o final, adicionando o fim da string (0)
+  dec rcx
+  mov byte[rcx], 0
+
+  ; Vamos sempre dividir rax / rdi, o resultado fica salvo novamente em rax
+  ; rax = rax / rdi
+  ; O resto da divisão vai para rdx
+  mov rax, rdi
+  mov rdi, 10
+
+.loop:
+  ; div combina rdx:rax para dividir um numero grande de 16 bytes
+  ; Como o rdx também é onde fica o resto da divisão, precisamos zerar ele antes de dividir novamente
+  xor rdx, rdx
+  div rdi
+
+  ; rdx vai conter o numero que 'extraimos' de rax
+  ; Podemos somar com o ASCII '0' e assim convertemos o numero em sua representação ASCII
+  add dl, '0'
+
+  ; Avançamos nosso indice para a proxima posição e movemos o valor ASCII obtido anteriomente para esse nova posição
+  dec rcx
+  mov byte[rcx], dl
+
+  ; Enquanto rax não for 0, ainda temos numero para contruir a string
+  cmp rax, 0
+  jne .loop
+
+.end:
+  ; rcx agora aponta para a string que construimos e podemos imprimir ela com a syscall WRITE
+  mov rdi, rcx
+  call print_string
+
+  ; Desalocamos os 21 bytes da stack
+  add rsp, 21
+  ret
+
 _start:
-  mov rdi, 'A'
-  call print_char
+  mov rdi, 0xabcdef
+  call print_uint
+  call print_newline
 
   xor rdi, rdi
   call exit
