d7ca5431caac3895b244893d8ac90787afeef17d
Erik Borela <erikborella@gmail.com>
2026-08-10 23:06:39 -0300

    Adicionado string_length à biblioteca de IO

--- assembly/io-lib/io-lib.asm
+++ assembly/io-lib/io-lib.asm
@@ -4,7 +4,7 @@
 %define STDOUT 1
 
 section .data
-message: db 'Hello, world!', 10
+message: db 'Hello, world!', 10, 0
 
 section .text
 global _start
@@ -14,7 +14,31 @@ exit:
   mov RAX, EXIT_SYSCALL
   syscall
 
-_start:
+; rdi = string
+; ret -> rax = tamanho da string
+string_length:
+  ; rax armazenará o tamanho da string
+  xor rax, rax
 
-  mov rdi, 7
+; Loop de contagem de caracteres não nulo (0)
+.loop:
+
+  ; Verifica se a posição atual da string sendo lida é o fim da string (0)
+  cmp byte [rdi+rax], 0
+  je .end
+
+  ; Continuamos incrementando o indice até achar o fim da string
+  inc rax
+
+  jmp .loop
+
+.end:
+  ; No fim, a posição que encontramos o fim da string será o tamanho dela
+  ret
+
+_start:
+  mov rdi, message
+  call string_length
+
+  mov rdi, rax
   call exit
